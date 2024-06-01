91ff0cd57dee8e327434d8e9cf889b04038f5682
Andrea <ancforeropi@gmail.com>
2024-06-01 11:24:48 -0500

    Mi aplicación Django, primer commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+def hi (name):
+    if name=="Daniel":
+        print("hola "+name)
+    elif name=="Eduardo":
+        print("hi "+name)
+    else:
+        print("by2, no eres daniel eduardo")
+        
+        
+
+hi("Eduardo")
